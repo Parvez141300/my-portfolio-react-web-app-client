66069797d4cb211a4aac147a2403dd837c7e6631
Parvez141300 <parvezhossain744471@gmail.com>
2025-08-13 00:18:53 +0600

    added some icons to the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
 import logo from "../../assets/logo/PHA Logo Design.png";
-import { RiMenu2Line, RiMenu3Fill } from "react-icons/ri";
+import {
+  RiContactsBook3Fill,
+  RiContactsFill,
+  RiMenu2Line,
+  RiMenu3Fill,
+} from "react-icons/ri";
 import DarkToggleButton from "./shared/DarkToggleButton";
 import MenuCard from "../MenuCard/MenuCard";
+import { FaHome } from "react-icons/fa";
+import { FaUserGear } from "react-icons/fa6";
+import { MdOutlineMenuBook } from "react-icons/md";
+import { GrProjects } from "react-icons/gr";
+import { IoMdDownload } from "react-icons/io";
+import { CgMoreVerticalO } from "react-icons/cg";
 
 const NavBar = () => {
   const menuRef = useRef(null);
@@ -26,7 +37,7 @@ const NavBar = () => {
 
   const handleSmoothScroll = (e, id) => {
     e.preventDefault();
-    setShow(false)
+    setShow(false);
     const element = document.getElementById(id);
     if (element) {
       element.scrollIntoView({
@@ -39,10 +50,16 @@ const NavBar = () => {
     <div className="w-10/12 mx-auto flex justify-between items-center relative">
       {/* menu icon & logo */}
       <div className="flex gap-2 items-center">
-        <button onClick={() => setShow(!show)} className="md:hidden btn btn-ghost px-0">
+        <button
+          onClick={() => setShow(!show)}
+          className="md:hidden btn btn-ghost px-0"
+        >
           <RiMenu2Line size={25} />
         </button>
-        <a onClick={(e) => handleSmoothScroll(e, 'banner')} className="cursor-pointer">
+        <a
+          onClick={(e) => handleSmoothScroll(e, "banner")}
+          className="cursor-pointer"
+        >
           <img src={logo} alt={logo} className="w-12" />
         </a>
       </div>
@@ -50,28 +67,39 @@ const NavBar = () => {
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a onClick={(e) => handleSmoothScroll(e, "banner")}>Home</a>
+            <a onClick={(e) => handleSmoothScroll(e, "banner")}>
+              <FaHome />
+              Home
+            </a>
           </li>
           <li>
-            <a onClick={(e) => handleSmoothScroll(e, "about")}>About</a>
+            <a onClick={(e) => handleSmoothScroll(e, "about")}>
+              <RiContactsFill />
+              About
+            </a>
           </li>
           <li>
-            <a onClick={(e) => handleSmoothScroll(e, "skills")}>Skills</a>
+            <a onClick={(e) => handleSmoothScroll(e, "skills")}>
+              <FaUserGear />
+              Skills
+            </a>
           </li>
           <li>
             <details>
-              <summary>More</summary>
+              <summary> <CgMoreVerticalO /> More</summary>
               <ul className="text-black">
                 <li>
                   <a
                     href="https://parvez-hossain-alif.netlify.app/assets/cv/parvez%20cv.pdf"
                     target="_blank"
                   >
+                    <IoMdDownload />
                     Download CV
                   </a>
                 </li>
                 <li>
                   <a onClick={(e) => handleSmoothScroll(e, "educations")}>
+                    <MdOutlineMenuBook />
                     Educations
                   </a>
                 </li>
@@ -79,10 +107,16 @@ const NavBar = () => {
             </details>
           </li>
           <li>
-            <a onClick={(e) => handleSmoothScroll(e, "projects")}>Projects</a>
+            <a onClick={(e) => handleSmoothScroll(e, "projects")}>
+              <GrProjects />
+              Projects
+            </a>
           </li>
           <li>
-            <a onClick={(e) => handleSmoothScroll(e, "contact")}>Contact</a>
+            <a onClick={(e) => handleSmoothScroll(e, "contact")}>
+              <RiContactsBook3Fill />
+              Contact
+            </a>
           </li>
         </ul>
       </div>
