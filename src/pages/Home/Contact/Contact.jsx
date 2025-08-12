d3e104c6d2f4cb0f1f1aee41cd7b3f97899178c3
Parvez141300 <parvezhossain744471@gmail.com>
2025-08-12 16:34:21 +0600

    edited contact spacing

--- src/pages/Home/Contact/Contact.jsx
+++ src/pages/Home/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
           Contact <span className="underline text-secondary">Me</span>
         </h2>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
         {/* Contact Card */}
         <div className="bg-gradient-to-t from-primary to-secondary border border-[#99999999] rounded-3xl p-5 space-y-3">
           <div>
