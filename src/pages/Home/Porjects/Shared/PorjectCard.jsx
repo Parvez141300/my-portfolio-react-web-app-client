280d5b37f8c7fd472bd09aefcf2bf500dbedc80d
Parvez141300 <parvezhossain744471@gmail.com>
2025-10-08 13:07:23 +0600

    added a package called react-tilty and addded the effect to the project card

--- src/pages/Home/Porjects/Shared/PorjectCard.jsx
+++ src/pages/Home/Porjects/Shared/PorjectCard.jsx
@@ -1,67 +1,82 @@
 import React from "react";
 import { Fade } from "react-awesome-reveal";
 import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
+import Tilty from "react-tilty";
 
 const ProjectCard = ({ project, handlePopup }) => {
   return (
     <Fade duration={1000}>
-      <div className="h-full flex flex-col rounded-xl overflow-hidden border border-primary/30  shadow-lg group hover:-translate-y-4 transition-all duration-500">
-        {/* Image */}
-        <div className="relative">
-          <img
-            src={project?.image}
-            alt={project?.title}
-            className="w-full h-52 object-cover transition-all duration-500 group-hover:brightness-50"
-          />
-          <span className="absolute top-2 right-2 bg-cyan-500 text-black px-2 py-1 rounded-md text-xs font-semibold">
-            {project?.category}
-          </span>
-        </div>
-
-        {/* Content */}
-        <div className="p-5 space-y-4">
-          {/* Title */}
-          <h2 className="text-lg font-bold">{project?.title}</h2>
-
-          {/* Tech Stack */}
-          <div className="flex flex-wrap gap-2">
-            {project.techStack.map((tech, index) => (
-              <span key={index} className="badge badge-outline badge-secondary">
-                {tech}
-              </span>
-            ))}
+      <Tilty
+        glare
+        maxGlare={0.5}
+        scale={1.05}
+        perspective={4000}
+        style={{ pointerEvents: "none" }}
+      >
+        <div
+          style={{ pointerEvents: "auto" }}
+          className="h-full flex flex-col rounded-xl overflow-hidden border border-primary/30  shadow-lg group"
+        >
+          {/* Image */}
+          <div className="relative">
+            <img
+              src={project?.image}
+              alt={project?.title}
+              className="w-full h-52 object-cover transition-all duration-500 group-hover:brightness-50"
+            />
+            <span className="absolute top-2 right-2 bg-cyan-500 text-black px-2 py-1 rounded-md text-xs font-semibold">
+              {project?.category}
+            </span>
           </div>
 
-          {/* see more button to open modal */}
-          <button
-            onClick={() => {
-              handlePopup(project.id);
-              document.getElementById("my_modal_3").showModal();
-            }}
-            className="hover:underline cursor-pointer"
-          >
-            See More...
-          </button>
+          {/* Content */}
+          <div className="p-5 space-y-4">
+            {/* Title */}
+            <h2 className="text-lg font-bold">{project?.title}</h2>
 
-          {/* Buttons */}
-          <div className="flex gap-3 pt-3">
-            <a
-              href={project?.links?.live}
-              target="_blank"
-              className="btn btn-primary btn-sm flex items-center gap-2"
+            {/* Tech Stack */}
+            <div className="flex flex-wrap gap-2">
+              {project.techStack.map((tech, index) => (
+                <span
+                  key={index}
+                  className="badge badge-outline badge-secondary"
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+
+            {/* see more button to open modal */}
+            <button
+              onClick={() => {
+                handlePopup(project.id);
+                document.getElementById("my_modal_3").showModal();
+              }}
+              className="hover:underline cursor-pointer"
             >
-              {project?.icons?.live} Live Demo
-            </a>
-            <a
-              href={project?.links?.code}
-              target="_blank"
-              className="btn btn-sm btn-outline btn-secondary flex items-center gap-2"
-            >
-              {project?.icons?.code} Code
-            </a>
+              See More...
+            </button>
+
+            {/* Buttons */}
+            <div className="flex gap-3 pt-3">
+              <a
+                href={project?.links?.live}
+                target="_blank"
+                className="btn btn-primary btn-sm flex items-center gap-2"
+              >
+                {project?.icons?.live} Live Demo
+              </a>
+              <a
+                href={project?.links?.code}
+                target="_blank"
+                className="btn btn-sm btn-outline btn-secondary flex items-center gap-2"
+              >
+                {project?.icons?.code} Code
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      </Tilty>
     </Fade>
   );
 };
